4788db30d1dcf8181a7f8430a7f64d323596c702
Andrey Ovechkin <andryu.ovechkin@gmail.com>
2025-10-21 19:50:28 +0300

    Add solution

---
+++ src/summ.rs
@@ -0,0 +1,15 @@
+use std::io;
+
+
+fn main() {
+	let mut input = String::new();
+	io::stdin().read_line(&mut input).expect("Reading error");
+
+	let nums: Vec<i32> = input
+		.trim()
+		.split_whitespace()
+		.map(|s| s.parse().expect("Error: user entered something other than a number"))
+		.collect();
+
+	println!("{}", nums[0] + nums[1]);
+}
