5be2c38a526dc7bc41eae75ef0d76c963696a30a
Andrey Ovechkin <andryu.ovechkin@gmail.com>
2025-10-25 21:27:57 +0300

    The file with the solution has been moved

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
-fn main() {
+use std::io;
 
+
+fn main() {
+	let mut input = String::new();
+	io::stdin().read_line(&mut input).expect("Reading error");
+
+	let nums: Vec<i32> = input
+		.trim()
+		.split_whitespace()
+		.map(|s| s.parse().expect("Error: user entered something other than a number"))
+		.collect();
+
+	print!("{}", nums[0] + nums[1]);
 }
